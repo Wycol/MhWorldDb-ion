55d6adc9eaf81a999996b93b1fbbcf767b82b0c4
Wycol <raulangulomar@gmail.com>
2020-08-16 20:07:05 +0200

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'monsters',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'monsters',
+    loadChildren: () => import('./monsters/monsters.module').then( m => m.MonstersPageModule)
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,37 +13,26 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Monsters',
+      url: '/monsters',
+      icon: '/assets/ion-custom-icons/monstericon.png'
     },
     {
       title: 'Outbox',
       url: '/folder/Outbox',
-      icon: 'paper-plane'
+      icon: '/assets/ion-custom-icons/monstericon.png'
     },
     {
       title: 'Favorites',
       url: '/folder/Favorites',
-      icon: 'heart'
+      icon: '/assets/ion-custom-icons/monstericon.png'
     },
     {
       title: 'Archived',
       url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
+      icon: '/assets/ion-custom-icons/monstericon.png'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
   constructor(
     private platform: Platform,
@@ -61,9 +50,5 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
-    }
   }
 }
